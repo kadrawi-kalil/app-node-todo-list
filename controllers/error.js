35c36ac71e19403f4cd31c87052db82ad7d5428f
kadrawi-kalil <khadhraouikhalil52@gmail.com>
2020-07-30 11:24:35 +0100

    update error.js

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,5 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).send(' error 404   try with postman http://localhost:3003/signup ');
+  res.status(404).send(' error 404  try to Sign in with   https://app-node-todo-kalil.herokuapp.com/signup ');
 };
 
 
